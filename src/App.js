6641f4ff41b917f73a243947ac3fde49a2f1b816
mingyuanhua <m.y.hua@hotmail.com>
2023-06-09 00:11:14 -0700

    Update App Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import { Layout } from "antd";
+import { Layout, Typography } from "antd";
+import { useState } from "react";
+import FoodList from "./components/FoodList";
+import LoginForm from "./components/LoginForm"; // default export
+import MyCart from "./components/MyCart";
+import SignupForm from "./components/SignupForm";
 
 const { Header, Content } = Layout;
+const { Title } = Typography;
 
+// fuction component
 function App() {
+  const [authed, setAuthed] = useState(false);
+
   return (
     <Layout style={{ height: "100vh" }}>
-      <Header>header</Header>
+      <Header>
+        <div className="header" style={{ display: "flex", justifyContent: "space-between" }}>
+          <Title
+            level={2}
+            style={{ color: "white", lineHeight: "inherit", marginBottom: 0 }}
+          >
+            Doordash Food
+          </Title>
+          <div>{authed ? <MyCart /> : <SignupForm />}</div>
+        </div>
+      </Header>
       <Content
         style={{
           padding: "50px",
@@ -13,7 +32,11 @@ function App() {
           overflowY: "auto",
         }}
       >
-        content
+        {authed ? (
+          <FoodList />
+        ) : (
+          <LoginForm onSuccess={() => setAuthed(true)} />
+        )}
       </Content>
     </Layout>
   );
